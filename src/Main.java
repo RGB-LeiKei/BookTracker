6f887b26cd2768e59a54316f2273ee3d54f1af2b
LeiKei <92036191+RGB-LeiKei@users.noreply.github.com>
2026-04-10 10:38:43 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@
 //  javac -cp "lib\\sqlite-jdbc-3.51.3.0.jar" src\\Main.java src\\DataLoader.java
 //  java -cp "lib\\sqlite-jdbc-3.51.3.0.jar;src" Main
 //
+// OR
+//
+// javac -cp lib/sqlite-jdbc-3.51.3.0.jar src/Main.java src/DataLoader.java
+// java -cp lib/sqlite-jdbc-3.51.3.0.jar:src Main
+//
 //  2. Run the run.bat file if you are on Windows, or run.sh if you are on Linux/Mac. These scripts will compile and run the code for you.
 
 import java.sql.*;
